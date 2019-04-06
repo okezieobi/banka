71f1b3d6529c2d99fcbfda99792c36a3fb9c57dc
okezieobi <obiedereokezie@gmail.com>
2019-04-06 14:50:35 +0100

    [Feature #164697688] Made more corrrections to develop branch

--- controllers/User/signIn.js
+++ controllers/User/signIn.js
@@ -1,6 +1,7 @@
 // @ts-ignore
 import logic from '../index';
 import data from '../../db';
+import services from '../../services';
 
 logic.userSignIn = () => {};
 
---
+++ services/index.js
@@ -0,0 +1,41 @@
+class Banka {
+  async validateEmail(email) {
+    this.emailPattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    return this.emailPattern.test(email);
+  }
+
+  async validatePassword(password) {
+    this.passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return this.passwordPattern.test(password);
+  }
+
+  async checkName(name) {
+    this.namePattern = /^[A-Za-z\s]+$/;
+    return this.namePattern.test(name);
+  }
+
+  findUser(array, param) {
+    this.foundUser = array.find(foundUser => foundUser.userEmail === param.userEmail);
+    return this.foundUser;
+  }
+
+  errorResponse(res, status, message) {
+    this.errRes = {
+      status,
+      message,
+    };
+    return res.send(this.errRes);
+  }
+
+  successResponse(res, status, data) {
+    this.successRes = {
+      status,
+      data,
+    };
+    res.send(this.successRes);
+  }
+}
+
+const banka = new Banka();
+
+export default banka;
