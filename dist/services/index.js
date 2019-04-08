88caad6c334a4bf5c289505c5e40fb02c70c767e
okezieobi <obiedereokezie@gmail.com>
2019-04-08 14:04:02 +0100

    [Chore #164697688] Updated services/index and db/index files

--- dist/services/index.js
+++ dist/services/index.js
@@ -5,10 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -24,117 +20,33 @@ function () {
 
   _createClass(Banka, [{
     key: "validateEmail",
-    value: function () {
-      var _validateEmail = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee(email) {
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                this.emailPattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-                return _context.abrupt("return", this.emailPattern.test(email));
-
-              case 2:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
-
-      function validateEmail(_x) {
-        return _validateEmail.apply(this, arguments);
-      }
-
-      return validateEmail;
-    }()
+    value: function validateEmail(email) {
+      this.emailPattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+      return this.emailPattern.test(email);
+    }
   }, {
     key: "validatePassword",
-    value: function () {
-      var _validatePassword = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee2(password) {
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
-          while (1) {
-            switch (_context2.prev = _context2.next) {
-              case 0:
-                this.passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-                return _context2.abrupt("return", this.passwordPattern.test(password));
-
-              case 2:
-              case "end":
-                return _context2.stop();
-            }
-          }
-        }, _callee2, this);
-      }));
-
-      function validatePassword(_x2) {
-        return _validatePassword.apply(this, arguments);
-      }
-
-      return validatePassword;
-    }()
+    value: function validatePassword(password) {
+      this.passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      return this.passwordPattern.test(password);
+    }
   }, {
     key: "checkName",
-    value: function () {
-      var _checkName = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee3(name) {
-        return regeneratorRuntime.wrap(function _callee3$(_context3) {
-          while (1) {
-            switch (_context3.prev = _context3.next) {
-              case 0:
-                this.namePattern = /^[A-Za-z\s]+$/;
-                return _context3.abrupt("return", this.namePattern.test(name));
-
-              case 2:
-              case "end":
-                return _context3.stop();
-            }
-          }
-        }, _callee3, this);
-      }));
-
-      function checkName(_x3) {
-        return _checkName.apply(this, arguments);
-      }
-
-      return checkName;
-    }()
+    value: function checkName(name) {
+      this.namePattern = /^[A-Za-z\s]+$/;
+      return this.namePattern.test(name);
+    }
   }, {
     key: "checkAmount",
-    value: function () {
-      var _checkAmount = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee4(amount) {
-        return regeneratorRuntime.wrap(function _callee4$(_context4) {
-          while (1) {
-            switch (_context4.prev = _context4.next) {
-              case 0:
-                this.amountPattern = /^[0-9]+$/;
-                return _context4.abrupt("return", this.amountPattern.test(amount));
-
-              case 2:
-              case "end":
-                return _context4.stop();
-            }
-          }
-        }, _callee4, this);
-      }));
-
-      function checkAmount(_x4) {
-        return _checkAmount.apply(this, arguments);
-      }
-
-      return checkAmount;
-    }()
+    value: function checkAmount(amount) {
+      this.amountPattern = /^[0-9]+$/;
+      return this.amountPattern.test(amount);
+    }
   }, {
     key: "findUser",
     value: function findUser(array, param) {
       this.foundUser = array.find(function (foundUser) {
-        return foundUser.userEmail === param.userEmail;
+        return foundUser.email === param.userEmail;
       });
       return this.foundUser;
     }
