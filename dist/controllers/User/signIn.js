f7514ec03a048d3b13aef96b9406373daf7b4f8d
okezieobi <obiedereokezie@gmail.com>
2019-04-09 20:39:30 +0100

    [Feature #164733676] Added endpoint that signs in Users

--- dist/controllers/User/signIn.js
+++ dist/controllers/User/signIn.js
@@ -14,7 +14,19 @@ var _services = _interopRequireDefault(require("../../services"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // @ts-ignore
-_index.default.userSignIn = function () {};
+_index.default.userSignIn = function (req, res) {
+  if (!req.body.userEmail) return _services.default.errorResponse(res, 400, 'Email is required');
+  if (!_services.default.validateEmail(req.body.userEmail)) return _services.default.errorResponse(res, 400, 'Email format is wrong');
+  if (!req.body.userPassword) return _services.default.errorResponse(res, 400, 'Password is required');
+  if (!_services.default.validatePassword(req.body.userPassword)) return _services.default.errorResponse(res, 400, 'Password must be eight characters minimum, at least one uppercase letter, one lowercase letter, one number and one special character');
+
+  var registeredUser = _services.default.findOne(_db.default.users, req.body, 'email', 'userEmail');
+
+  if (!registeredUser) return _services.default.errorResponse(res, 400, 'User does not exist, please sign up');
+  if (registeredUser.password !== req.body.userPassword) return _services.default.errorResponse(res, 400, 'Password does not match user');
+  delete registeredUser.password;
+  return _services.default.successResponse(res, 200, registeredUser);
+};
 
 var _default = _index.default.userSignIn;
 exports.default = _default;
