2081fa90c60cd3e53909aa239460c05466ee2ddb
okezieobi <obiedereokezie@gmail.com>
2019-04-11 18:08:18 +0100

    [Feature #164733796] Update controller for create a bank account

--- controllers/User/createBankAccount.js
+++ controllers/User/createBankAccount.js
@@ -4,13 +4,13 @@ import data from '../../db';
 import services from '../../services';
 
 logic.createBankAccount = (req, res) => {
-  if (!req.body.ownerId) return services.errorResponse(res, 400, 'User Id is required');
-  if (!services.checkNumber(req.body.ownerId)) return services.errorResponse(res, 400, 'User id must be numbers');
+  if (!req.headers['owner-id']) return services.errorResponse(res, 400, 'User Id is required');
+  if (!services.checkNumber(req.headers['owner-id'])) return services.errorResponse(res, 400, 'User id must be numbers');
   if (!req.body.bankAccountType) return services.errorResponse(res, 400, 'Bank account type is required');
   if (!services.checkName(req.body.bankAccountType)) return services.errorResponse(res, 400, 'Bank account type must be letters');
   if (req.body.bankAccountType !== 'current' && req.body.bankAccountType !== 'savings'
     && req.body.bankAccountType !== 'Current' && req.body.bankAccountType !== 'Savings') return services.errorResponse(res, 400, 'Bank account type must be savings or current');
-  const findUser = services.findById(data.users, req.body, 'id', 'ownerId');
+  const findUser = services.findById(data.users, req.headers, 'id', 'owner-id');
   if (!findUser) return services.errorResponse(res, 400, 'Only registered users can create bank accounts, please sign up');
   const newBankAccount = data.bankAccount(req.body);
   const bankAccountRes = data.createBankAccountResponse(newBankAccount, findUser);
