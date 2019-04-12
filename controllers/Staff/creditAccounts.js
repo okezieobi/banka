7f790f2e6714f6946ab76ac1b1a2de1de5434312
okezieobi <obiedereokezie@gmail.com>
2019-04-12 12:04:14 +0100

    [Feature #164734354] Added endpoint and tests to credit a bank account as a signed in Staff

--- controllers/Staff/creditAccounts.js
+++ controllers/Staff/creditAccounts.js
@@ -12,7 +12,7 @@ logic.creditAccount = (req, res) => {
   const findAccountNumber = services.findById(data.bankAccounts, req.params, 'accountNumber', 'account_number');
   const verifyCashier = services.findById(data.staff, req.headers, 'id', 'cashier-id');
   if (!findAccountNumber) return services.errorResponse(res, 404, 'Account number not found');
-  if (!verifyCashier) return services.errorResponse(res, 404, 'Staff not found, only registered staff can debit or credit an bank account');
+  if (!verifyCashier) return services.errorResponse(res, 404, 'Staff not found, only registered staff can debit or credit a bank account');
   req.params.accountBalance = findAccountNumber.balance;
   const newTransaction = data.creditAccountTransaction(req.body, req.params, req.headers);
   const responseTransaction = data.transactionResponse(newTransaction);
