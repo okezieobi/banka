d05a262d022a4dbcc458d989801b1d426361d9bb
okezieobi <obiedereokezie@gmail.com>
2019-04-13 11:24:23 +0100

    [Feature #164733991] Added controller logic

--- controllers/Admin/toggleAccounts.js
+++ controllers/Admin/toggleAccounts.js
@@ -3,6 +3,18 @@ import logic from '../index';
 import data from '../../db';
 import services from '../../services';
 
-logic.toggleAccountState = () => {};
+logic.toggleAccountState = ( req, res ) => {
+    if (!req.body.accountStatus) return services.errorResponse(res, 400, 'Account status is required');
+    if (!services.checkName( req.body.accountStatus)) return services.errorResponse(res, 400, 'Account status must be letters');
+    if (req.body.accountStatus !== 'active' || req.body.accountStatus !== 'Active' ||
+        req.body.accountStatus !== 'dormant' || req.body.accountStatus !== 'Dormant') return services.errorResponse(res, 400, 'Account status must equal active or dormant');
+    if (!req.headers['admin-id']) return services.errorResponse(res, 400, 'Admin id is required');
+    if (!services.checkNumber(req.headers['admin-id'])) return services.errorResponse(res, 400, 'Admin id must be numbers');
+    if (!services.findById(data.admins, req.headers, 'id', 'admin-id')) return services.errorResponse(res, 404, 'Admin id not found, only registered admins can update an account detail');
+    if (!services.checkNumber(req.params.account_number)) return services.errorResponse(res, 400, 'Account number must be a number');
+    const bankAccount = services.findById(data.bankAccounts, req.params, 'accountNumber', 'account_number');
+    if (!bankAccount) return services.errorResponse(res, 404, 'Account number not found');
+    bankAccount.status = req.body.accountStatus;
+};
 
 export default logic.toggleAccountState;
