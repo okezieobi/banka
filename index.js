38b48263d2bd8e47735f02958e1965a0752a1d89
okezieobi <obiedereokezie@gmail.com>
2019-04-19 17:48:04 +0100

    [Chore #164697688] Updated port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import app from './app';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || '3000';
 app.listen(port, () => {
   console.log(`App is live and listening on port ${port}!`);
 });
