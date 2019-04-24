a952d6306d2ce271ee4e4fa1bc1e2ad2dfb55527
okezieobi <obiedereokezie@gmail.com>
2019-04-25 00:01:08 +0100

    [Chore #164697688] Updated test.js and package.json to order tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,14 @@ import chai, {
 import chaiHttp from 'chai-http';
 import app from '../src';
 
+require('./users/signIn');
+require('./users/signUp');
+require('./accounts/createBankAccounts');
+require('./transactions/debitAccounts');
+require('./transactions/creditAccounts');
+require('./accounts/updateAccountStatus');
+require('./accounts/deleteAccount');
+
 export {
   expect,
   chai,
